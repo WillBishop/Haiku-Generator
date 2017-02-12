c789d06e1d31db2cd95dc1c5cc1c2aca614d0889
Will Bishop <WillBishop@users.noreply.github.com>
2017-02-12 14:21:21 +1030

    Added more cards to it

--- Haiku.py
+++ Haiku.py
@@ -4,8 +4,8 @@ import tkinter
 from tkinter import *
 root = Tk()
 root.geometry('{}x{}'.format(170, 100))
-top_line = ["The heart of a child", "Edward Scissor-Hands", "2 Kids, One Sandbox", "Check yo privilege", "doing the right thing", "Mad Hacky-Sack Skills"]
-middle_line = ["Teaching a robot to love", "The worlds worst human being", "A dingo ate my baby", "A child beauty pageant", "The Make-A-Wish Foundation", "Michael Jackson"]
+top_line = ["The heart of a child", "Edward Scissor-Hands", "2 Kids, One Sandbox", "Check yo privilege", "doing the right thing", "Mad Hacky-Sack Skills", "Being on fire", "Same-Sex ice dancing", "The forbidden fruit", "A salty surprise", "Morgan Freeman's voice", "Parting the red sea"]
+middle_line = ["Teaching a robot to love", "The worlds worst human being", "A dingo ate my baby", "A child beauty pageant", "The Make-A-Wish Foundation", "Michael Jackson", "The profoundly handicapped", "A middle aged man on roller skates", ]
 bottom_line = ["Teenage pregnancy", "Kids with ass cancer", "Apologizing", "Being Fabulous", "Public Ridicule"]
 def generate():
     top_line_choice = str(random.choice(top_line))
